ec44d145921fb4fa0877acfe47821dccd7408be0
chiangshin <jiangxin792@gmail.com>
2020-04-28 14:51:05 +0800

    fix

--- config-server/src/main/java/com/chiangshin/springcloud/ConfigServerApplication.java
+++ config-server/src/main/java/com/chiangshin/springcloud/ConfigServerApplication.java
@@ -1,18 +1,15 @@
 package com.chiangshin.springcloud;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-/**
- * @Author jx
- * @Date 2020/4/25 23:27
- */
+@EnableConfigServer
 @SpringBootApplication
 @EnableEurekaClient
-public class DemoServerApplication {
+public class ConfigServerApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DemoServerApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
 }
--- demo-server/src/main/java/com/chiangshin/springcloud/test/HelloController.java
+++ demo-server/src/main/java/com/chiangshin/springcloud/test/HelloController.java
@@ -1,14 +1,23 @@
 package com.chiangshin.springcloud.test;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-	
+	@Value("${msg.info}")
+    private String msg;
+
     @RequestMapping("/hello")
     public String index(@RequestParam(required = false) String name) {
         return "hello "+name+"，this is first messge";
+    }
+
+    @RequestMapping("/getMsg")
+    public String getMsg(){
+        return "msg value:"+this.msg;
     }
 }
